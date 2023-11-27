425e7f34be184768bba2e246d1426b9f2da8f4c3
https://gitlab.com/chapin2012 <chapin2012@hotmail.com>
2023-11-26 21:05:04 -0500

    Created github account and added first file and markdown file

---
+++ MyFirstFile.js
@@ -0,0 +1,10 @@
+// !preview r2d3 data=c(0.3, 0.6, 0.8, 0.95, 0.40, 0.20)
+//
+// r2d3: https://rstudio.github.io/r2d3
+//
+
+print("This file was created within Rstudio")
+print("And now it lives on Gitlab")
+
+
+print("Now lets for this repo on git lab")
